dcb057eedb27e09e57f9131992fa147d1e4b2a6e
Adenikym <mm5adeniky@gmail.com>
2020-08-10 16:25:22 +0100

    frontend

---
+++ user module/index.js
@@ -0,0 +1,17 @@
+function addDetails () {
+document.getElementById("bt").addEventListener("click", function () {
+    
+    document.querySelector(".bg-modal").style.display="flex"
+
+   
+})
+    
+}
+
+addDetails();
+
+document.querySelector(".sub").addEventListener("click", function() {
+    document.querySelector(".bg-modal").style.display="none";
+    
+})
+
